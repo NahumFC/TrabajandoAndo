5ef9bbbdfaf77b0c9e3b427b1aecf9462bfa33d8
NahumFC <nahumfc2003@gmail.com>
2023-06-13 13:08:28 -0600

    Actualización de ejs

--- index.js
+++ index.js
@@ -11,5 +11,14 @@ app.use(express.static('HTML'));
 /** Rutas */
 app.get('/', (req, res) => res.render('index'))
 app.get('/login', (req, res) => res.render('login'))
+app.get('/perfilA', (req, res) => res.render('perfilA'))
+app.get('/principalA', (req, res) => res.render('principalA'))
+app.get('/perfilE', (req, res) => res.render('perfilE'))
+app.get('/principalE', (req, res) => res.render('principalE'))
+app.get('/administradorempleos', (req, res) => res.render('administradorempleos'))
+app.get('/administradorempresa', (req, res) => res.render('administradorempresa'))
+app.get('/administradorinicio', (req, res) => res.render('administradorinicio'))
+app.get('/administradorsolicitantes', (req, res) => res.render('administradorsolicitantes'))
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
