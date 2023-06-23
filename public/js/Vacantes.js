787545582b9a8a387baceea9b5383e286db4aafc
NahumFC <nahumfc2003@gmail.com>
2023-06-22 23:27:40 -0600

    Actualización vista final

--- public/js/Vacantes.js
+++ public/js/Vacantes.js
@@ -11,7 +11,22 @@
     }
 
 
+     // Obtener elementos
+     var botones = document.getElementsByClassName('acept-button');
+     var avisos = document.getElementsByClassName('aviso2');
+ 
+     for (var i = 0; i < botones.length; i++) {
+       botones[i].addEventListener('click', function() {
+         var aviso = this.nextElementSibling;
+         aviso.innerText = 'Felicidades, se aceptó correctamente';
+         aviso.style.display = 'block';
+       });
+     }
+
+    function verPerfilE() {
+      window.location.href = 'perfilE-Alumno';
+    }
 
     function verPerfilA() {
-      window.location.href = 'perfilA-Empresa.html';
+      window.location.href = 'perfilA-Empresa';
     }
